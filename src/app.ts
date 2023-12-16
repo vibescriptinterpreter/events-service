bd04fef8dd4313d86c00d2ab223af46a77064c7a
Oganes Aganyan <oganes.aganyan@simbirsoft.com>
2023-12-16 18:13:40 +0300

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
